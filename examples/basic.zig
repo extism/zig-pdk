20e5c61d5fc2f8737f7d380d993dc2c293ef300e
Steve Manuel <steve@dylib.so>
2022-12-27 15:47:23 -0700

    feat: use json in basic example, add comments

--- examples/basic.zig
+++ examples/basic.zig
@@ -6,6 +6,13 @@ const http = zig_pdk.http;
 pub fn main() void {}
 const allocator = std.heap.wasm_allocator;
 
+// define some type to write as output from the plugin back to the host
+const Output = struct {
+    count: i32,
+    config: []const u8,
+    a: []const u8,
+};
+
 export fn count_vowels() i32 {
     const plugin = pdk.init(allocator);
     const input = plugin.getInput() catch unreachable;
@@ -17,6 +24,8 @@ export fn count_vowels() i32 {
             else => {},
         }
     }
+
+    // use persistent variables owned by a plugin instance (stored in-memory between function calls)
     var var_a_optional = plugin.getVar("a") catch unreachable;
     if (var_a_optional == null) {
         plugin.setVar("a", "this is var a");
@@ -26,11 +35,13 @@ export fn count_vowels() i32 {
     const var_a = plugin.getVar("a") catch unreachable orelse "";
     defer allocator.free(var_a);
 
+    // access host-provided configuration (key/value)
     const thing = plugin.getConfig("thing") catch unreachable orelse "<unset by host>";
 
-    const output = std.fmt.allocPrint(allocator, "{{\"count\": {d}, \"config\": \"{s}\", \"a\": \"{s}\"}}", .{ count, thing, var_a }) catch unreachable;
+    const output = std.json.stringifyAlloc(allocator, Output{ .count = count, .config = thing, .a = var_a }, .{}) catch unreachable;
     defer allocator.free(output);
 
+    // write the plugin data back to the host
     plugin.output(output);
 
     return 0;
@@ -38,14 +49,19 @@ export fn count_vowels() i32 {
 
 export fn make_http_request() i32 {
     const plugin = pdk.init(allocator);
+    // create an HTTP request via Extism built-in function (doesn't require WASI)
     var req = http.HttpRequest.init(allocator, "GET", "https://jsonplaceholder.typicode.com/todos/1");
+    defer req.deinit();
+
+    // set headers on the request object
     req.setHeader("some-name", "some-value") catch unreachable;
     req.setHeader("another", "again") catch unreachable;
 
-    defer req.deinit();
+    // make the request and get the response back
     const res = plugin.request(req) catch unreachable;
     defer res.deinit();
 
+    // `outputMemory` provides a zero-copy way to write plugin data back to the host
     plugin.outputMemory(res.memory);
 
     return 0;
