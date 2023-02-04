779d8f9b52a83bf01d8aa0d779af9d41a683be85
Doğu Us <uspro@disroot.org>
2023-02-04 14:08:20 +0300

    fix: Update for breaking build system changes

--- build.zig
+++ build.zig
@@ -1,26 +1,29 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
-    const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .freestanding } });
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{ 
+        .default_target = .{ .abi = .musl, .os_tag = .freestanding, .cpu_arch = .wasm32 },
+    });
 
-    const lib = b.addStaticLibrary("extism-pdk", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.addIncludePath("src/");
+    const lib = b.addStaticLibrary(.{
+        .name = "extism-pdk",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     lib.install();
 
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
-
-    var basic_example = b.addExecutable("Basic example", "examples/basic.zig");
+    var basic_example = b.addExecutable(.{
+        .name = "Basic example",
+        .root_source_file = .{ .path = "examples/basic.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     basic_example.addPackagePath("extism-pdk", "src/main.zig");
-    basic_example.setTarget(target);
-    basic_example.setBuildMode(mode);
+    basic_example.rdynamic = true;
     basic_example.setOutputDir("examples-out");
 
     const basic_example_step = b.step("basic_example", "Build basic_example");
