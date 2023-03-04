57a5c73d7451ab2665cf3745184e6a1b82448937
Doğu Us <uspro@disroot.org>
2023-03-04 15:30:35 +0300

    feat: initial zig package manager support

--- build.zig
+++ build.zig
@@ -1,20 +1,36 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
+    comptime {
+        const current_zig = builtin.zig_version;
+        const min_zig = std.SemanticVersion.parse("0.11.0-dev.1817+f6c934677") catch return; // package manager hashes made consistent on windows
+        if (current_zig.order(min_zig) == .lt) {
+            @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
+        }
+    }
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const optimize = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{ 
+    const target = b.standardTargetOptions(.{
         .default_target = .{ .abi = .musl, .os_tag = .freestanding, .cpu_arch = .wasm32 },
     });
 
-    const lib = b.addStaticLibrary(.{
+    b.addModule(.{
         .name = "extism-pdk",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
+        .source_file = .{ .path = "src/main.zig" },
     });
-    lib.install();
+
+    // const pdk_module = b.createModule(.{
+    //     .source_file = .{ .path = "src/main.zig" },
+    // });
+    // const lib = b.addStaticLibrary(.{
+    //     .name = "extism-pdk",
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // lib.install();
 
     var basic_example = b.addExecutable(.{
         .name = "Basic example",
@@ -22,6 +38,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    // basic_example.addModule("extism-pdk", pdk_module);
     basic_example.addAnonymousModule("extism-pdk", .{ .source_file = .{ .path = "src/main.zig" } });
     basic_example.rdynamic = true;
     basic_example.setOutputDir("examples-out");
