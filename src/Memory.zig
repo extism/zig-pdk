85c4d0754beba0565e0aa49e3ac0c09a75d14e44
Muhammad Azeez <muhammad@dylibso.com>
2023-11-22 08:20:55 +0300

    Revert "fix: s/little/Little on endian enum"
    This reverts commit 119acb03f79ab821297729ce3c4acee757cc8a98.

--- src/Memory.zig
+++ src/Memory.zig
@@ -25,7 +25,7 @@ pub fn load(self: Self, buf: []u8) void {
         }
 
         const x = extism.load_u64(self.offset + @as(u64, i));
-        std.mem.writeInt(u64, buf[i..][0..8], x, std.builtin.Endian.Little);
+        std.mem.writeInt(u64, buf[i..][0..8], x, std.builtin.Endian.little);
         i += 8;
     }
 }
@@ -39,7 +39,7 @@ pub fn store(self: Self, buf: []const u8) void {
             i += 1;
             continue;
         }
-        const data = std.mem.readInt(u64, buf[i..][0..8], std.builtin.Endian.Little);
+        const data = std.mem.readInt(u64, buf[i..][0..8], std.builtin.Endian.little);
         extism.store_u64(self.offset + @as(u64, i), data);
         i += 8;
     }
