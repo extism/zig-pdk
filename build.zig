42b0b856a71469d5ec931fd539eabf26ec6317f1
Doğu Us <73913444+usdogu@users.noreply.github.com>
2024-05-22 01:01:48 +0300

    fix: build (#30)

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ const builtin = @import("builtin");
 pub fn build(b: *std.Build) void {
     comptime {
         const current_zig = builtin.zig_version;
-        const min_zig = std.SemanticVersion.parse("0.12.0-dev.2030+2ac315c24") catch unreachable;
+        const min_zig = std.SemanticVersion.parse("0.13.0-dev.230+50a141945") catch unreachable; // build system changes: ziglang/zig#19597
         if (current_zig.order(min_zig) == .lt) {
             @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
         }
@@ -17,14 +17,14 @@ pub fn build(b: *std.Build) void {
 
     var basic_example = b.addExecutable(.{
         .name = "basic-example",
-        .root_source_file = .{ .path = "examples/basic.zig" },
+        .root_source_file = b.path("examples/basic.zig"),
         .target = target,
         .optimize = optimize,
     });
     basic_example.rdynamic = true;
     basic_example.entry = .disabled; // or, add an empty `pub fn main() void {}` in your code
     const pdk_module = b.addModule("extism-pdk", .{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
     });
     basic_example.root_module.addImport("extism-pdk", pdk_module);
 
